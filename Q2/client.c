eec711e724a3025dc0094aaa5ca10b9b9a316c00
Kashif-Kamran <i190541@nu.edu.pk>
2021-10-17 22:44:24 +0500

    Question # 1 part 1,2,3 has been done

--- Q2/client.c
+++ Q2/client.c
@@ -11,11 +11,17 @@
 #include <arpa/inet.h>
 #define PORT 9999
 #define MAXLINE 1024
+
 int main()
 {
+    printf("\n_______ Client Side _______\n\n");
     /* ------------------ Variables to Send and Receive the msg ----------------- */
     char msgSend[MAXLINE];
     char msgRecv[MAXLINE];
+    char str1[MAXLINE];
+    char str2[MAXLINE];
+    char choice[5];
+
     int sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0)
     {
@@ -26,12 +32,55 @@ int main()
     serverAdress.sin_family = AF_INET;
     serverAdress.sin_addr.s_addr = INADDR_ANY;
     serverAdress.sin_port = htons(PORT);
-    printf("Enter msg to send : ");
-    gets(msgSend);
     connect(sockfd, (struct sockaddr *)&serverAdress, sizeof(serverAdress));
-    send(sockfd, msgSend, sizeof(msgSend), 0);
+    printf("Enter msg to send : ");
+    gets(choice);
+    send(sockfd, choice, sizeof(choice), 0);
     recv(sockfd, msgRecv, sizeof(msgRecv), 0);
-    printf("msgRecv : %s", msgRecv);
+    printf("[__ %s __]\n", msgRecv);
+    if (!strcmp(choice, "1"))
+    {
+        printf("Enter First String  : ");
+        gets(str1);
+        printf("Enter Second String  : ");
+        gets(str2);
+        send(sockfd, str1, sizeof(str1), 0);
+        send(sockfd, str2, sizeof(str2), 0);
+        recv(sockfd, msgRecv, sizeof(msgRecv), 0);
+        printf("Result : %s\n", msgRecv);
+    }
+    else if (!strcmp(choice, "2"))
+    {
+        printf("Enter String : ");
+        gets(str1);
+        printf("Enter Character : ");
+        gets(str2);
+        send(sockfd, str1, sizeof(str1), 0);
+        send(sockfd, str2, sizeof(str2), 0);
+        recv(sockfd, msgRecv, sizeof(msgRecv), 0);
+        printf("Result : %s\n", msgRecv);
+    }
+    else if (!strcmp(choice, "3"))
+    {
+        printf("Enter String : ");
+        gets(str1);
+        printf("Enter Character : ");
+        gets(str2);
+        send(sockfd, str1, sizeof(str1), 0);
+        send(sockfd, str2, sizeof(str2), 0);
+        recv(sockfd, msgRecv, sizeof(msgRecv), 0);
+        printf("Result : %s\n", msgRecv);
+    }
+    else if (!strcmp(choice, "4"))
+    {
+    }
+    else if (!strcmp(choice, "5"))
+    {
+    }
+    else
+    {
+    }
+
     close(sockfd);
     return 0;
 }
--- Q2/server.c
+++ Q2/server.c
@@ -1,5 +1,6 @@
 #include <unistd.h>
 #include <stdio.h>
+#include <ctype.h>
 #include <sys/types.h>
 #include <errno.h>
 #include <sys/wait.h>
@@ -11,13 +12,54 @@
 #include <arpa/inet.h>
 #define PORT 9999
 #define MAXLINE 1024
+void manu()
+{
+    printf("\n1. For Cancatination of Strings\n");
+    printf("2. For Occurance of Characters\n");
+    printf("3. For Comparing Two Strings \n");
+    printf("4. For Checking Palindrom String\n");
+    printf("5. For Adding String Numbers\n\n");
+}
+int stringsComparison(char *str1, char *str2)
+{
+    int i = 0;
+    while (str1[i] != '\0' && str2[i] != '\0')
+    {
+        if (tolower(str1[i]) != tolower(str2[i]))
+        {
+            return 0;
+        }
+        i++;
+    }
+    return 1;
+}
+int occuranceOfChar(char *str, char ch)
+{
+    int i = 0;
+    int index = -1;
+    while (str[i] != '\0')
+    {
+        if (str[i] == ch)
+        {
+            printf("i : %d", i);
+            index = i;
+            break;
+        }
+        i++;
+    }
+    return index;
+}
 int main()
 {
     printf("\n_______ Server Side _______\n\n");
 
     /* ----------------------- Variables to Store the Data ---------------------- */
-    char msgSend[MAXLINE] = "I got you msg";
+    char msgSend[MAXLINE];
     char msgRecv[MAXLINE];
+    char choice[MAXLINE];
+    char str1[MAXLINE];
+    char str2[MAXLINE];
+    int occurance;
 
     /* -------------------------------------------------------------------------- */
     /*                               Connection Code                              */
@@ -45,6 +87,7 @@ int main()
     /* -------------------------------------------------------------------------- */
     /*                            Welcomeing Port Code                            */
     /* -------------------------------------------------------------------------- */
+    manu();
     while (1)
     {
         int clientSocket = accept(server_socket, NULL, NULL);
@@ -52,10 +95,60 @@ int main()
         pid_t pid = fork();
         if (pid == 0)
         {
-            printf("new process Created");
-            recv(clientSocket, msgRecv, sizeof(msgRecv), 0);
-            printf("Msg Received : %s\n", msgRecv);
-            send(clientSocket, msgSend, sizeof(msgSend), 0);
+            recv(clientSocket, choice, sizeof(choice), 0);
+            printf("Choice Received : %s\n", choice);
+            if (!strcmp(choice, "1"))
+            {
+                send(clientSocket, "Send Two Strings For Cancatination", 36, 0);
+                recv(clientSocket, str1, sizeof(str1), 0);
+                recv(clientSocket, str2, sizeof(str2), 0);
+                printf("String 1 : %s\n", str1);
+                printf("String 2 : %s\n", str2);
+                strcat(str1, str2);
+                strcpy(msgSend, str1);
+                send(clientSocket, msgSend, sizeof(msgSend), 0);
+            }
+            else if (!strcmp(choice, "2"))
+            {
+                send(clientSocket, "Send a string and a Character to find its Occurance", 53, 0);
+                recv(clientSocket, str1, sizeof(str1), 0);
+                recv(clientSocket, str2, sizeof(str2), 0);
+                printf("String : %s\n", str1);
+                printf("Character : %c\n", str2[0]);
+                occurance = occuranceOfChar(str1, str2[0]);
+                printf("Occurance : %d\n", occurance);
+                sprintf(msgSend, "%d", occurance);
+            }
+            else if (!strcmp(choice, "3"))
+            {
+                send(clientSocket, "Send Two Strings For comparison", 33, 0);
+                recv(clientSocket, str1, sizeof(str1), 0);
+                recv(clientSocket, str2, sizeof(str2), 0);
+                printf("String 1 : %s\n", str1);
+                printf("String 2 : %s\n", str2);
+                int check = stringsComparison(str1, str2);
+                if (check == 0)
+                {
+                    strcpy(msgSend, "False");
+                }
+                else
+                {
+                    strcpy(msgSend, "True");
+                }
+                send(clientSocket, msgSend, sizeof(msgSend), 0);
+            }
+            else if (!strcmp(choice, "4"))
+            {
+                send(clientSocket, "Send One String to find whether it is palindrom or not", 56, 0);
+            }
+            else if (!strcmp(choice, "5"))
+            {
+                send(clientSocket, "Send Two string values to find sum", 36, 0);
+            }
+            else
+            {
+                send(clientSocket, "____ Invalid Choice Entered ____", 34, 0);
+            }
         }
     }
     close(server_socket);
